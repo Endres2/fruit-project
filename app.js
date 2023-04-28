c77ef4ea68a5f13728c1f2283b162f9988558d09
Endres2 <andresdhr99@gmail.com>
2023-04-27 23:19:33 -0400

    Add mongoose to the project, conect to MongoDB and create two collections Fruits and Persons, save a record for each

--- app.js
+++ app.js
@@ -4,62 +4,36 @@
  
 // ----- 2nd cmd ----- 
 // (1)mongod
+const mongoose = require("mongoose")
  
-const { MongoClient } = require("mongodb");
- 
-// Connection URL (Replace the url string with your connection string.)
-// const url = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
-const url = "mongodb://localhost:27017";
- 
-// Create a new MongoClient
-const client = new MongoClient(url);
- 
-async function main() {
-  try {
-    // ----- Use connect method to connect to the server ----- 
-    await client.connect();
-    console.log('Connected successfully to server');
-    
-    // ----- Database Name ----- 
-    const database = client.db('fruitsDB');
-    // ----- Create a collection ----- 
-    const fruitsCollection = database.collection('fruits');
- 
-    // ----- Insert a document (data in row) -----
-    // Create an array of documents to insert
-    const docs = [
-        {
-            name: "Apple",  
-            score: 8, 
-            review: "Great Fruit"},
-        {   
-            name: "Orange", 
-            score: 6, 
-            review: "Kinda Sour"},
-        {
-            name: "Banana", 
-            score: 9, 
-            review: "Great Stuff"}
-       ];
-     
-    const insertResult = await fruitsCollection.insertMany(docs);
-    console.log("Inserted " + insertResult.insertedCount + " documents into the fruitsCollection.");
-    
-    // ----- 3rd cmd (to check whether the data is in mongosh) -----  
-    // (1)mongosh 
-    // (2)show dbs 
-    // (3)use <databseName>
-    // (4)show collections
-    // (5)db.<collectionName>.find()
-    
-    // ----- Find all documents -----
-    const findResult = await fruitsCollection.find({}).toArray();
-    console.table(findResult);
- 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
- 
-main().catch(console.dir);
+mongoose.connect("mongodb://localhost:27017/fruitsDB").then(() => console.log('Connected!'));
+
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+})
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+    name:"Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit."
+})
+
+//fruit.save();
+
+const personSchema = new mongoose.Schema({
+    name:String,
+    age:Number
+})
+
+const Person = mongoose.model("Person",personSchema )
+
+const person = new Person({
+    name:"John",
+    age: 37
+})
+
+person.save()
